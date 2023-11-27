61fbf74c2504f2cf9d9f69215d79a7c839252896
David Amador <david.amador@enti.cat>
2023-11-27 10:11:44 +0100

    Pasos hasta el 16 que no se guardaron

--- EFTP/client.sh
+++ EFTP/client.sh
@@ -5,7 +5,8 @@ IP=`ip address | grep inet | grep enp0s3 | cut -c 10-19`
 echo $IP
 
 SERVER="localhost"
-PORT="$PORT"
+PORT="3333"
+FILE=""
 
 echo "Cliente de EFTP"
 
@@ -20,7 +21,7 @@ echo $DATA
 echo "(5) Test & send"
 if [ "$DATA" != "OK_HEADER" ]
 then
-echo "ERROR 33: HEADER NO COINCIDE"
+echo "ERROR 2: HEADER NO COINCIDE"
 exit 2
 fi
 
@@ -31,6 +32,51 @@ echo "BOOM" | nc $SERVER $PORT
 echo "(6) Listen"
 DATA=`nc -l -p $PORT -w 0`
 echo $DATA
+
+echo "(9) Test"
+if [ "$DATA" == "KO_HANDSHAKE" ]
+then
+	echo "ERROR 3: BAD HANDSHAKE"
+	exit 2
+fi
+
+echo "(10) Send"
+#Enviar el archivo
+sleep 1
+echo "FILE_NAME fary1.txt"  | nc $SERVER $PORT
+
+echo "(11) Listen"
+DATA=`nc -l -p $PORT -w 0`
+
+echo "(14) Test & Send"
+if [ "$DATA" == "KO_FILE_NAME" ]
+then
+	echo "ERROR 4: WRONG FILE NAME"
+	exit 3
+fi
+
+sleep 1
+cat imgs/fary1.txt | nc $SERVER $PORT
+
+echo "(15) Listen"
+DATA=`nc -l -p $PORT -w 0`
+
+if [ "$DATA" != "OK_DATA" ]
+then
+	echo "ERROR 5: BAD DATA"
+	exit 4
+fi
+
+echo "FIN"
+exit 0
+
+
+
+
+
+
+
+
 
 
 
--- EFTP/servidor.sh
+++ EFTP/servidor.sh
@@ -28,11 +28,64 @@ DATA=`nc -l -p $PORT -w 0`
 
 echo $DATA
 
+echo "(7) Test & send"
 if [ "$DATA" != "BOOM" ]
 then
-	echo "ERROR 2: BAD HANDSHAKE"
-
+	echo "ERROR 3: BAD HANDSHAKE"
 	sleep 1
 	echo "KO_HANDSHAKE" | nc $CLIENT $PORT
 	exit 2
 fi
+
+echo "(8) Listen"
+DATA=`nc -l -p $PORT -w 0`
+echo $DATA
+
+echo "(12) Test & store & send"
+
+PREFIX=`echo $DATA | cut -d " " -f 1`
+
+if [ "$PREFIX" != "FILE_NAME" ]
+then
+	echo "ERROR 4: WRONG FILE_NAME"
+	sleep 1
+	echo "KO_FILE_NAME" | nc $CLIENT $PORT
+	exit 3
+fi
+
+FILE_NAME=`echo $DATA | cut -d " " -f 2`
+echo "OK_FILE_NAME"
+sleep 1
+echo "OK_FILE_NAME" | nc $CLIENT $PORT
+
+echo "(13) Listen"
+DATA=`nc -l -p $PORT -w 0`
+
+echo "(16) Store & send"
+if [ "$DATA" == "" ]
+then
+	echo "ERROR 4: BAD FILE NAME PREFIX"
+	sleep 1
+	echo "KO_DATA" | nc $CLIENT $PORT
+	exit 4
+fi
+
+echo $DATA > inbox/$FILE_NAME
+
+sleep 1
+echo "OK_DATA" | nc $CLIENT $PORT
+
+echo "FIN"
+exit 0
+
+
+
+
+
+
+
+
+
+
+
+
