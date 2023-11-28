da8ca09d3f74a74c988099c400cab1ad077937d8
David Amador <david.amador@enti.cat>
2023-11-28 11:18:39 +0100

    Comprobaciones de MD5

--- EFTP/client.sh
+++ EFTP/client.sh
@@ -1,38 +1,41 @@
 #!/bin/bash
 
-IP=`ip address | grep inet | grep enp$TIMEOUTs3 | cut -c 1TIMEOUT-19`
+IP=`ip address | grep inet | grep enp0s3 | cut -c 10-19`
 
 echo $IP
 
 SERVER="localhost"
 PORT="3333"
-TIMEOUT="1"
-FILE=""
 
 echo "Cliente de EFTP"
 
 echo "(1) send"
 echo "EFTP 1.0" | nc $SERVER $PORT
 
+
 echo "(2) Listen"
-DATA=`nc -l -p $PORT -w $TIMEOUT`
+DATA=`nc -l -p $PORT -w 0`
 
 echo $DATA
+
 
 echo "(5) Test & send"
 if [ "$DATA" != "OK_HEADER" ]
 then
-	echo "ERROR 2: HEADER NO COINCIDE"
-	exit 2
+echo "ERROR 2: HEADER NO COINCIDE"
+exit 2
 fi
 
-echo "BOOOM"
+echo "BOOM"
 sleep 1
-echo "BOOOM" | nc $SERVER $PORT
+echo "BOOM" | nc $SERVER $PORT
+
 
 echo "(6) Listen"
-DATA=`nc -l -p $PORT -w $TIMEOUT`
+DATA=`nc -l -p $PORT -w 0`
+
 echo $DATA
+
 
 echo "(9) Test"
 if [ "$DATA" == "KO_HANDSHAKE" ]
@@ -41,13 +44,19 @@ then
 	exit 2
 fi
 
-echo "(10) Send"
+
 #Enviar el archivo
+echo "(10) Send"
+MD5=`echo $FILE | md5sum | cut -d " " -f 1`
+FILE="fary1.txt"
+
 sleep 1
-echo "FILE_NAME fary1.txt"  | nc $SERVER $PORT
+echo "FILE_NAME $FILE $MD5"  | nc $SERVER $PORT
+
 
 echo "(11) Listen"
-DATA=`nc -l -p $PORT -w $TIMEOUT`
+DATA=`nc -l -p $PORT -w 0`
+
 
 echo "(14) Test & Send"
 if [ "$DATA" == "KO_FILE_NAME" ]
@@ -59,14 +68,26 @@ fi
 sleep 1
 cat imgs/fary1.txt | nc $SERVER $PORT
 
+
 echo "(15) Listen"
-DATA=`nc -l -p $PORT -w $TIMEOUT`
+DATA=`nc -l -p $PORT -w 0`
 
 if [ "$DATA" != "OK_DATA" ]
 then
 	echo "ERROR 5: BAD DATA"
 	exit 4
 fi
+
+echo "(18) Send"
+FILE_MD5=`cat $FILE | md5sum | cut -d " " -f 1`
+
+echo "FILE_MD5"
+sleep 1
+echo "FILE_MD5 $FILE_MD5" | nc $SERVER $PORT
+
+
+echo "(19) Listen"
+DATA=`nc -l -p $PORT -w 0`
 
 echo "FIN"
 exit 0
--- EFTP/servidor.sh
+++ EFTP/servidor.sh
@@ -11,6 +11,7 @@ DATA=`nc -l -p $PORT -w $TIMEOUT`
 
 echo $DATA
 
+
 echo "(3) Test & Send"
 if [ "$DATA" != "EFTP 1.0" ]
 then
@@ -24,10 +25,12 @@ echo "OK_HEADER"
 sleep 1
 echo "OK_HEADER" | nc $CLIENT $PORT
 
+
 echo "(4) Listen"
 DATA=`nc -l -p $PORT -w $TIMEOUT`
 
 echo $DATA
+
 
 echo "(7) Test & send"
 if [ "$DATA" != "BOOOM" ]
@@ -41,43 +44,92 @@ fi
 sleep 1
 echo "OK_HANDSHAKE" | nc $CLIENT $PORT
 
+
 echo "(8) Listen"
 DATA=`nc -l -p $PORT -w $TIMEOUT`
 echo $DATA
 
-echo "(12) Test & store & send"
 
+echo "(12) Test & store & send"
 PREFIX=`echo $DATA | cut -d " " -f 1`
 
 if [ "$PREFIX" != "FILE_NAME" ]
 then
-	echo "ERROR 4: WRONG FILE_NAME"
+	echo "ERROR 4: BAD PREFIX"
 	sleep 1
 	echo "KO_FILE_NAME" | nc $CLIENT $PORT
 	exit 3
 fi
 
 FILE_NAME=`echo $DATA | cut -d " " -f 2`
+
 echo "OK_FILE_NAME"
 sleep 1
 echo "OK_FILE_NAME" | nc $CLIENT $PORT
+
+#COMPROBAR MD5
+MD5_CLIENT=`echo $DATA | cut -d " " -f 3`
+MD5_TEST=`echo $FILE_NAME | md5sum | cut -d " " -f 1`
+
+if [ "$MD5" != "$MD5_TEST" ]
+then
+	echo "ERROR 5: BAD MD5"
+	sleep 1
+	echo "KO_MD5" | nc $CLIENT $PORT
+fi
+
+echo "OK_MD5"
+sleep 1
+echo "OK_MD5" | nc $CLIENT $PORT
+
 
 echo "(13) Listen"
 DATA=`nc -l -p $PORT -w $TIMEOUT`
 
 echo "(16) Store & send"
-if [ "$DATA" == "" ]
+DATA_FILE=`cat $FILE_NAME`
+
+if [ $DATA_FILE == "" ]
 then
-	echo "ERROR 4: BAD FILE NAME PREFIX"
+	echo "KO_DATA"
 	sleep 1
 	echo "KO_DATA" | nc $CLIENT $PORT
-	exit 4
 fi
 
 echo $DATA > inbox/$FILE_NAME
 
 sleep 1
 echo "OK_DATA" | nc $CLIENT $PORT
+
+
+echo "(17) Listen"
+DATA=`nc -l -p $PORT -w $TIMEOUT`
+
+
+echo "(20) Test & Send"
+#COMPROBAMOS EL PREFIX
+PREFIX=`echo $DATA | cut -d " " -f 1`
+
+if [ "$PREFIX" != "FILE_MD5" ]
+then
+	echo "BAD_PREFIX"
+	sleep 1
+	echo "KO_PREFIX" | nc $CLIENT $PORT
+fi
+
+sleep 1
+echo "OK_PREFIX" | nc $CLIENT $PORT
+
+#COMPROBAMOS EL MD5
+FILE_MD5_CLIENT=`echo $DATA | cut -d " " -f 2`
+FILE_MD5_SERVER=`cat $DATA_FILE | md5sum | cut -d " " -f 1`
+
+if [ $FILE_MD5_CLIENT != $FILE_MD5_SERVER ]
+then
+	echo "KO_FILE_MD5"
+	sleep 1
+	echo "KO_FILE_MD5" | nc $CLIENT $PORT
+fi
 
 echo "FIN"
 exit 0
