e1249394fe53d722a4f96a3853b5b01c02751562
songorren02 <151615638+songorren02@users.noreply.github.com>
2023-12-12 15:09:31 +0100

    Update servidor.sh

--- EFTP/servidor.sh
+++ EFTP/servidor.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-CLIENT="localhost"
 PORT="3333"
-TIMEOUT="1"
+TIMEOUT=1
 
 echo "Servidor de EFTP"
 
@@ -12,6 +11,7 @@ DATA=`nc -l -p $PORT -w $TIMEOUT`
 echo $DATA
 
 
+#Comprovamos por separado si el header está bien
 echo "(3) Test & Send"
 PREFIX=`echo $DATA | cut -d " " -f 1`
 VERSION=`echo $DATA | cut -d " " -f 2`
@@ -51,6 +51,7 @@ DATA=`nc -l -p $PORT -w $TIMEOUT`
 echo $DATA
 
 
+#Comprueba si el handshake está bien
 echo "(7) Test & send"
 if [ "$DATA" != "BOOOM" ]
 then
@@ -69,6 +70,8 @@ DATA=`nc -l -p $PORT -w $TIMEOUT`
 
 echo $DATA
 
+
+#Comprueba si el prefijo es correctp y la cantidad de archivos que va a enviar
 echo "(7b) Test & Send OK/KO NUM_FILES"
 PREFIX=`echo $DATA | cut -d " " -f 1`
 
@@ -86,17 +89,22 @@ echo "OK_NUM_FILES" | nc $CLIENT $PORT
 
 NUM_FILES=`echo $DATA | cut d " " -f 2`
 
+
+#Empezamos el bucle for, la cantidad de veces como cantidad de archivos tiene el cliente
+echo "(7c) Loop" 
 for NUM in `seq $NUM_FILES`
 do
-
 echo "Archivo numero $NUM"
+
 
 echo "(8) Listen"
 DATA=`nc -l -p $PORT -w $TIMEOUT`
 echo $DATA
 
 
+#Comprueba el prefijo y el nombre del archivo. Luego se lo guarda en inbox
 echo "(12) Test & store & send"
+#COMPROBAR PREFIX
 PREFIX=`echo $DATA | cut -d " " -f 1`
 
 if [ "$PREFIX" != "FILE_NAME" ]
@@ -107,21 +115,21 @@ then
 	exit 3
 fi
 
-FILE_NAME=`echo $DATA | cut -d " " -f 2`
-
 echo "OK_FILE_NAME"
 sleep 1
 echo "OK_FILE_NAME" | nc $CLIENT $PORT
 
 #COMPROBAR MD5
+FILE_NAME=`echo $DATA | cut -d " " -f 2`
 MD5_CLIENT=`echo $DATA | cut -d " " -f 3`
 MD5_TEST=`echo $FILE_NAME | md5sum | cut -d " " -f 1`
 
-if [ "$MD5" != "$MD5_TEST" ]
+if [ "$MD5_CLIENT" != "$MD5_TEST" ]
 then
-	echo "ERROR 5: BAD MD5"
+	echo "ERROR 5: BAD FILE_MD5"
 	sleep 1
-	echo "KO_MD5" | nc $CLIENT $PORT
+	echo "KO_FILE_MD5" | nc $CLIENT $PORT
+ 	exit 4
 fi
 
 echo "OK_MD5"
@@ -129,21 +137,28 @@ sleep 1
 echo "OK_MD5" | nc $CLIENT $PORT
 
 
+#Escuchar el contenido del archivo en cuestión y guardarlo en inbox/Archivo (Se crea en ese momento)
 echo "(13) Listen"
-DATA=`nc -l -p $PORT -w $TIMEOUT`
+#Guardar en inbox lo que nos llega. 
+> [!IMPORTANT]
+> PREGUNTAR SOBRE INBOX A RAFA
+#DATA=`nc -l -p $PORT -w $TIMEOUT`
+nc -l -p $PORT -w $TIMEOUT > inbox/$FILE_NAME
 
+
+#Comprobamos el interior del archivo y hacemos un reporte
 echo "(16) Store & send"
-DATA_FILE=`cat $FILE_NAME`
+DATA_FILE=`cat inbox/$FILE_NAME`
 
 if [ $DATA_FILE == "" ]
 then
 	echo "ERROR 6: KO_DATA"
 	sleep 1
 	echo "KO_DATA" | nc $CLIENT $PORT
+ 	exit 4
 fi
 
-echo $DATA > inbox/$FILE_NAME
-
+echo "OK_DATA" 
 sleep 1
 echo "OK_DATA" | nc $CLIENT $PORT
 
@@ -161,6 +176,7 @@ then
 	echo "ERROR 7: BAD_PREFIX"
 	sleep 1
 	echo "KO_FILE_MD5" | nc $CLIENT $PORT
+ 	exit 5
 fi
 
 sleep 1
@@ -168,18 +184,19 @@ echo "OK_PREFIX" | nc $CLIENT $PORT
 
 #COMPROBAMOS EL MD5
 FILE_MD5_CLIENT=`echo $DATA | cut -d " " -f 2`
-FILE_MD5_SERVER=`cat $DATA_FILE | md5sum | cut -d " " -f 1`
+FILE_MD5_SERVER=`$DATA_FILE | md5sum | cut -d " " -f 1`
 
 if [ "$FILE_MD5_CLIENT" != "$FILE_MD5_SERVER" ]
 then
 	echo "KO_FILE_MD5"
 	sleep 1
 	echo "KO_FILE_MD5" | nc $CLIENT $PORT
+ 	exit 6
 fi
 
-echo "OK_FILE_MD5"
+echo "OK_DATA_FILE_MD5"
 sleep 1
-echo "OK_FILE_MD5" | nc $CLIENT $PORT
+echo "OK_DATA_FILE_MD5" | nc $CLIENT $PORT
 
 done
 
